e8832c6dc11731c2455ad2479dc14cb8a559bda2
ParthWadhwa14 <parthwadhwa1980@gmail.com>
2026-01-12 23:43:54 +0530

    hyper parameter tuning done

--- src/utils.py
+++ src/utils.py
@@ -16,23 +16,46 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
            
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for model_name, model in models.items():
-            model.fit(X_train, y_train)
+            
+            logging.info(f"Training {model_name}")
 
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            param_grid = params.get(model_name, {})
 
+            if param_grid:
+                gs = GridSearchCV(
+                    estimator=model,
+                    param_grid=param_grid,
+                    cv=3,
+                    scoring="r2",
+                    n_jobs=-1
+                )
+                gs.fit(X_train, y_train)
+
+                best_model = gs.best_estimator_
+            else:
+                best_model = model
+                best_model.fit(X_train, y_train)
+
+            y_test_pred = best_model.predict(X_test)
             test_score = r2_score(y_test, y_test_pred)
 
-            report[model_name] = test_score  # ✅ STORE SCORE ONLY
+            report[model_name] = test_score
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
+
 
     
